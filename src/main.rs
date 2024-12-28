72c87671869631d063fb36ea3ee35d6c582f7c9d
Niko Böckerman <git.585wj@slmail.me>
2024-12-28 11:28:28 +0000

    Add initial cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
+use clap::{Args, Parser, Subcommand};
+use clap_verbosity_flag::Verbosity;
+
+#[macro_use]
+extern crate log;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(flatten)]
+    verbose: Verbosity,
+
+    #[command(subcommand)]
+    command: Commands
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    All,
+    Day(DayArgs),
+    Single(SingleArgs),
+}
+
+#[derive(Args)]
+struct DayArgs {
+    day: u8,
+}
+
+#[derive(Args)]
+struct SingleArgs {
+    day: u8,
+    part: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    simple_logger::init_with_level(cli.verbose.log_level().unwrap()).unwrap();
+
+    match &cli.command {
+        Commands::All => {
+            println!("TODO: Run all solutions");
+        },
+        Commands::Day(day_args) => {
+            println!("TODO: Run day {}", day_args.day);
+        },
+        Commands::Single(single_args) => {
+            println!("TODO: Run single: day {} part {}", single_args.day, single_args.part);
+        },
+    }
+
+    trace!("TRACE");
+    debug!("DEBUG");
+    info!("INFO");
+    warn!("WARN");
+    error!("ERROR");
 }
